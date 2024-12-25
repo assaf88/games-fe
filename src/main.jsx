2a491c1fe556aaa4cf83036a20c8487960882c71
assaf88 <assaf.88@gmail.com>
2024-12-25 05:10:28 +0200

    try service worker for re-routing non index requests to index

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,17 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
+if (!process.env.PRODUCTION && 'serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+            console.error('Service Worker registration failed:', error);
+        });
+}
 
 createRoot(document.getElementById('root')).render(
     //<StrictMode> causes duplicate ws conn (dev only)
