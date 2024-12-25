9f05689c80d0134fd10c14d49d671111478cb3ea
assaf88 <assaf.88@gmail.com>
2024-12-25 11:51:37 +0200

    Revert "try service worker for re-routing non index requests to index"
    This reverts commit b9866c45bff32f6d06d6816b2f5b5ecd8a4d966c.

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,8 @@ const App = () => {
                 <Route exact path="/" element={<GameListPage />} />
                 <Route path="/avalon" element={<AvalonHomePage />} />
                 <Route path="/avalon/party/:id" element={<AvalonPartyPage />} />
-
-                <Route component={NotFound} />
             </Routes>
         </Router>
-    );
-};
-
-const NotFound = () => {
-    return (
-        <div>
-            <h1>Page Not Found</h1>
-            <p>Sorry, the page you are looking for does not exist.</p>
-        </div>
     );
 };
 
