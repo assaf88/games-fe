f8fb6dfc224394e40f2657e2fcaa76b832dfb6a0
assaf88 <assaf.88@gmail.com>
2025-08-02 20:00:50 +0300

    * next stage in avalon party page pre game start - selecting specials - not final BE missing 3 - host notes

--- src/AvalonPreGameSetup.jsx
+++ src/AvalonPreGameSetup.jsx
@@ -48,7 +48,8 @@ const AvalonPreGameSetup = ({
                     maxWidth: '510px',
                     // padding: '1rem', 
                     // padding: '0.6rem 0.5rem 0.6rem 0.8rem', 
-                    padding: `${isVertical ? '1.2% 1% 0.7% 1.1%' : '2.5% 3% 3% 4%'}`, //top, right, bottom, left
+                    padding: `${isVertical ? '1.7% 1.4% 1.3% 1.85%' : '2.5% 3% 3% 4%'}`, //top, right, bottom, left
+                    // padding: `${isVertical ? '1.2% 1% 0.7% 1.1%' : '2.5% 3% 3% 4%'}`, //top, right, bottom, left
                     // padding: `${isVertical ? '0.6rem 0.5rem 0.6rem 0.8rem' : '0.7rem 0.8rem 1rem 1.3rem'}`, 
                     background: 'rgba(27,22,21,0.8)', 
                     borderRadius: '10px',
