d6932734e369b73884d5458e787817d591dd891a
assaf88 <assaf.88@gmail.com>
2025-08-03 10:36:13 +0300

    * next stage in avalon party page pre game start - selecting specials - not final BE missing 5 - adjust player list 10 players size

--- src/GameParty.jsx
+++ src/GameParty.jsx
@@ -372,7 +372,7 @@ const GameParty = () => {
                                 {rightAmountOfPlayers ? (
                                     <>
                                         {isHost ? (
-                                        <div style={{display: 'flex', justifyContent: 'center', marginTop: `${isVertical ? '0.8rem' : '1.5rem'}`, scale: `${isVertical ? '0.90' : '1'}`}}>
+                                        <div style={{display: 'flex', justifyContent: 'center', marginTop: `${isVertical ? '0.4rem' : '1.5rem'}`, scale: `${isVertical ? '0.90' : '1'}`}}>
                                             <button className={isAvalon ? 'button' : ''} onClick={sendStartGame}>Start Game</button>
                                         </div>
                                         ) : (
